3fa8d95425a7247ffd9144b7616c8b0407e79932
jpkrooney <jrooney@rcsi.ie>
2026-04-04 11:09:32 +0200

    added nutpie files

---
+++ R/nutpieR_to_rstan_obj.R
@@ -0,0 +1,160 @@
+
+
+trim_pars <- function(nutfit, pars, exclude = TRUE){
+    allpars <- attributes(nutfit)$dimnames[[3]]
+    dims <- attributes(nutfit)$dim
+
+    idx <- lapply(1:length(pars), function(i) {
+        pat <- paste("^", pars[i], "(\\[(\\d+,)*\\d+\\])*$", sep = "")
+        grepl(pat, allpars) }) |>
+        bind_cols() |>
+        rowMeans() > 0
+
+    # If exclude = TRUE invert index
+    if (exclude == TRUE) { idx <- !idx}
+
+    # Apply the exclusions to nutfit
+    trimfit <- nutfit[ , , idx ]
+
+    attributes(trimfit)$diagnostics <- attributes(nutfit)$diagnostics
+    attributes(trimfit)$num_chains <- attributes(nutfit)$num_chains
+    attributes(trimfit)$num_warmup <- attributes(nutfit)$num_warmup
+    attributes(trimfit)$num_draws <- attributes(nutfit)$num_draws
+
+    return(trimfit)
+}
+
+
+
+
+
+
+
+
+make_sim_object <- function(nutfit) {
+
+    num_warmup <- attributes(nutfit)$num_warmup
+    num_draws <- attributes(nutfit)$num_draws
+
+    # Get diagnostics from nutfit
+    diagnostics <- nutpie_diagnostics(nutfit)
+
+    # add lp__ and repackage samples
+    lp__ <- array(diagnostics$logp, dim = c(dim(nutfit)[1], dim(nutfit)[2], 1),
+                  dimnames = c(dimnames(nutfit)[1:2],
+                               list("lp__")))
+    samples <- posterior::as_draws_list(abind::abind(nutfit, lp__, along = 3))
+    names(samples) <- NULL
+    class(samples) <- NULL
+
+    # rest of code adapted from rstan::read_stan_csv()
+    n <- length(diagnostics$diverging)
+    chains <- attr(diagnostics, "num_chains") %||% 1L
+    n_per_chain <- n %/% chains
+
+    fnames <- names(samples[[1]])
+    fnames <- rstan:::sqrfnames_to_dotfnames(fnames)
+    n_save <- nrow(samples[[1]])
+    paridx <- rstan:::paridx_fun(fnames)
+    lp__idx <- attr(paridx, "meta")["lp__"]
+    par_fnames <- c(fnames[paridx], "lp__")
+    pars_oi <- rstan:::unique_par(par_fnames)
+    dims_oi <- lapply(pars_oi, function(i) {
+        pat <- paste("^", i, "(\\.\\d+)*$", sep = "")
+        i_fnames <- par_fnames[grepl(pat, par_fnames)]
+        rstan:::get_dims_from_fnames(i_fnames, i)
+    })
+    names(dims_oi) <- pars_oi
+
+    sim <- list(samples = samples,
+               iter = num_warmup + num_draws,
+               thin = 1,
+               warmup = num_warmup,
+               chains = chains,
+               n_save = rep(num_draws, chains),
+               #warmup2 = rep(attributes(nutfit)[[1]][[1]], chains), # not sure about this line
+               warmup2 = rep(0, chains), # not sure about this line
+               permutation = lapply(1:chains, function(id) 1:num_draws),
+               pars_oi = pars_oi,
+               dims_oi = dims_oi,
+               fnames_oi = rstan:::dotfnames_to_sqrfnames(par_fnames),
+               n_flatnames = length(par_fnames))
+
+    return(sim)
+}
+
+
+
+nutpieR_to_rstan_obj <- function(nutfit, stanmodel){
+    # make sim object
+    sim <- make_sim_object(nutfit)
+
+    # make a stan_args list
+    stan_args <- lapply(1 : sim$chains, function(i) {
+        list(chain_id = i, iter = sim$iter, warmup = sim$warmup,
+        thin = 1, seed = NULL, algorithm = "NUTS", engine = "nutpieR") } )
+
+    # bundle into stanfit object
+    stanfit <- new("stanfit",
+                model_name = "",
+                model_pars = sim$pars_oi,
+                par_dims = sim$dims_oi,
+                mode = as.integer(0),
+                sim = sim,
+                inits = list(),
+                stan_args = stan_args,
+                stanmodel = stanmodel,
+                date = "")
+    ## add divergences etc ??
+
+    return(stanfit)
+}
+
+
+
+
+
+
+# fragile hacky function to make a nutpieR fit object into a stanfit object
+
+#nutpieR_to_rstan_obj <- function(nutfit, emptyrstan, num_draws, num_warmup){
+#
+#    attribs <- attributes(nutfit)
+#    diagnostics <- nutpie_diagnostics(nutfit)
+#
+#    n <- length(diagnostics$diverging)
+#    chains <- attr(diagnostics, "num_chains") %||% 1L
+#    n_per_chain <- n %/% chains
+#    n_save <- nrow(nutfit)
+#
+#
+#    lp__ <- array(diagnostics$logp, dim = c(dim(nutfit)[1], dim(nutfit)[2], 1),
+#                  dimnames = c(dimnames(nutfit)[1:2],
+#                               list("lp__")))
+#    samples <- posterior::as_draws_list(abind::abind(nutfit, lp__, along = 3))
+#    names(samples) <- NULL
+#    class(samples) <- NULL
+#    #for(i in 1:chains){
+#    #    attributes(samples[[i]])  <- attributes(emptyrstan@sim$samples[[i]])
+#    #}
+#
+#    # Extract structure
+#    fit <- emptyrstan
+#
+#    # Replace draws
+#    fit@sim$samples <- samples
+#    fit@sim$iter <- num_draws + num_warmup
+#    fit@sim$n_save <- rep(num_draws, chains)
+#    fit@sim$warmup <- num_warmup
+#    fit@sim$warmup2 <- rep(num_warmup, chains)
+#
+#    fit@sim$n_save <- rep(num_draws, chains)
+#    fit@sim$warmup2 <- attribs[[1]][[1]] - rep(num_warmup, chains)
+#
+#    # set attributes
+#    attributes(fit@sim$samples[[1]])$args$warmup <- as.integer(num_warmup)
+#    attributes(fit@sim$samples[[1]])$args$iter <- as.integer(num_draws + num_warmup)
+#    #attr(fit@sim$samples[[1]], 'sampler_params')$accept_stat__ <- diagnostics$mean_tree_accept
+#    #attr(fit@sim$samples[[1]], 'sampler_params')$stepsize__  <- diagnostics$step_size_bar
+#    return(fit)
+#}
---
+++ R/nutpie_fits.R
@@ -0,0 +1,247 @@
+library(tidyverse)
+library(brms)
+library(ggdist)
+library(rstan)
+library(patchwork)
+library(furrr)
+library(nutpieR)
+
+#options(mc.cores = 8, brms.backend = "cmdstanr")
+cache_dir <- here::here("local_temp_data", "simulation_test")
+if(!dir.exists(cache_dir)) {
+    dir.create(cache_dir, recursive = TRUE)
+}
+source(here::here("R", "pp_checks.R"))
+source(here::here("R", "mv_probit.R"))
+source(here::here("R", "simulate_data.R"))
+source(here::here("R", "sampling_parallel.R"))
+source(here::here("R", "brm_parallel.R"))
+
+source(here::here("R", "nutpieR_to_rstan_obj.R"))
+
+theme_set(cowplot::theme_cowplot())
+irldat <- read_csv(here::here("Data/longdata.csv"))
+
+irldat_full <- irldat %>%
+    rename("subject_id" = "ID",
+           "Q01" = "ALSFRS_Q1",
+           "Q02" = "ALSFRS_Q2",
+           "Q03" = "ALSFRS_Q3",
+           "Q04" = "ALSFRS_Q4",
+           "Q05" = "ALSFRS_Q5",
+           "Q06" = "ALSFRS_Q6",
+           "Q07" = "ALSFRS_Q7",
+           "Q08" = "ALSFRS_Q8",
+           "Q09" = "ALSFRS_Q9",
+           "Q10" = "ALSFRS_Q10",
+           "Q11" = "ALSFRS_Q11",
+           "Q12" = "ALSFRS_Q12",
+           "alsfrs_total" = "Total") %>%
+    dplyr::select(subject_id, alsfrs_dly_mnths, Q01, Q02, Q03, Q04, Q05, Q06,
+                  Q07, Q08, Q09, Q10, Q11, Q12, alsfrs_total)
+
+question_col_names_irldat <- sort(names(irldat_full)[grepl("^[QR]", names(irldat_full))])
+question_col_names <- sprintf("Q%02d", 1:12)
+question_col_names_translation <- sprintf("Q%02d", 1:12)
+names(question_col_names_translation) <- question_col_names_irldat
+
+irldat_standardized <- irldat_full
+
+
+
+
+M <- 100
+set.seed(99)
+
+questions_to_fit <- question_col_names
+sv <- make_stanvars_mv_probit_bgoodri(questions_to_fit)
+
+eff_probs <- rep(c(0.5), times = M)
+
+
+irldat_formula <- as.formula(paste0(
+    "mvbind(", paste0(questions_to_fit, collapse = ", "),
+    ") ~ 1 + alsfrs_dly_mnths*group + (1 | p | subject_id)"
+))
+
+#irldat_formula <- as.formula(paste0(
+#    "mvbind(", paste0(questions_to_fit, collapse = ", "),
+#    ") ~ 1 + alsfrs_dly_mnths*group + (1 | subject_id)"
+#))
+
+
+
+
+ls_dat <- lapply(1:length(eff_probs), function(m)
+    simulate_data_from_registry(irldat_standardized |>
+                                    group_by(subject_id) |>
+                                    filter(first(alsfrs_dly_mnths < 30)),
+                            max_duration = 24,
+                            min_measurements_per_subject = 2,
+                            max_measurements_per_subject = 8,
+                            n_subjects_per_group = 50,
+                            effect_prob = eff_probs[m]))
+
+
+ls_dat <- ls_dat |>
+    map(\(dat) dat |>
+            mutate(across(all_of(question_col_names),
+                  \(x) { x + 1 })))
+
+
+stancode <- brms::make_stancode(bf(irldat_formula, family = empty_cumulative()) +
+                                    set_rescor(FALSE), stanvars = sv, adapt_delta = 0.95, init = 0.0,
+                                data = ls_dat[[1]])
+stanmodel <- stan_model(model_code = stancode)
+
+
+# format data for stan
+ls_standat <- ls_dat |>
+    map(\(dat)
+        brms::make_standata(bf(irldat_formula, family = empty_cumulative()) +
+                                set_rescor(FALSE), stanvars = sv, adapt_delta = 0.95, init = 0.0,
+                            data = dat))
+
+
+#sink(file = "modelfornutpie.stan", )
+model <- nutpie_compile_model(code = stancode)
+
+# Sample
+plan(multisession, workers=2)
+nutfits <- ls_standat |>
+    future_map( \(dat)
+         nutpie_sample(model,
+                       data = dat,
+                       num_warmup = 500,
+                       num_draws = 250,
+                       num_chains = 4,
+                       cores = 4,
+                       refresh = 0,
+                       seed = 60471,
+                       init_mean = 0) |>
+             trim_pars(pars = "u"), # exclude nuisance pars to save memory
+         .progress = TRUE, .options = furrr_options(seed = 123))
+plan(sequential)
+
+##plan(multisession, workers=8)
+#brmsfits <- future_map(1:length(eff_probs), \(i) {
+brmsfits <- lapply(1: length(eff_probs), function(i) {
+        # currently need to fit empty obejcts for each fit - this is slow
+        #emptyrstan <- rstan::sampling(stanmodel, data = ls_standat[[i]],
+        #                          chains = 4, iter = 1, cores = 4, refresh = 0,
+        #                          init = 0.0, control = list(adapt_delta = 0.95))
+        emptybrms <- brm(bf(irldat_formula, family = empty_cumulative()) +
+                         set_rescor(FALSE), stanvars = sv, adapt_delta = 0.95, init = 0.1,
+                     data = ls_dat[[i]], empty = TRUE)
+        #
+        rstanobj <- nutpieR_to_rstan_obj(nutfits[[i]], stanmodel)
+        brmsfit <- emptybrms
+        brmsfit$fit <- rstanobj
+        brmsfit <- rename_pars(brmsfit)
+        brmsfit}) #, .progress = TRUE, .options = furrr_options(seed = 123))
+
+saveRDS(brmsfits, paste0(cache_dir, "/", "20260402_nutpiemvprobit_fits_test.RDS"))
+
+
+###### Reload fits for plotting ######
+brmsfits1 <- readRDS(paste0(cache_dir, "/", "20260402_nutpiemvprobit_fits_18mnths_n50.RDS"))
+brmsfits2 <- readRDS(paste0(cache_dir, "/", "20260402_nutpiemvprobit_fits_24mnths_n50.RDS"))
+brmsfits3 <- readRDS(paste0(cache_dir, "/", "20260402_nutpiemvprobit_fits_30mnths_n50.RDS"))
+brmsfits4 <- readRDS(paste0(cache_dir, "/", "20260402_nutpiemvprobit_fits_18mnths_n50_no_p.RDS"))
+brmsfits5 <- readRDS(paste0(cache_dir, "/", "20260402_nutpiemvprobit_fits_24mnths_n50_no_p.RDS"))
+brmsfits6 <- readRDS(paste0(cache_dir, "/", "20260402_nutpiemvprobit_fits_30mnths_n50_no_p.RDS"))
+
+
+#### define an effect function ####
+effect_from_sim_study <- function(fit) {
+    predict_data <- data.frame(group = c("Control", "Control", "Treatment", "Treatment"),
+                               alsfrs_dly_mnths = c(0, 12, 0, 12))
+
+    pred <- posterior_epred(fit, newdata = predict_data, re_formula = NA)
+
+    expected_sum <- function(x) {
+        col_values <- as.integer(colnames(x))
+        multiplied <- sweep(x, MARGIN = 2, STATS = col_values, FUN = "*")
+        rowSums(multiplied)
+    }
+
+    start_control <- expected_sum(pred[,1,])
+    end_control <- expected_sum(pred[,2,])
+    start_treatment <- expected_sum(pred[,3,])
+    end_treatment <- expected_sum(pred[,4,])
+
+    (end_treatment - start_treatment) - (end_control - start_control)
+}
+
+
+#### Summarise effects ####
+summaries_mv_probit1 <- brmsfits1 |>
+    map(\(fit) effect_from_sim_study(fit))
+summaries_mv_probit2 <- brmsfits2 |>
+    map(\(fit) effect_from_sim_study(fit))
+summaries_mv_probit3 <- brmsfits3 |>
+    map(\(fit) effect_from_sim_study(fit))
+summaries_mv_probit4 <- brmsfits4 |>
+    map(\(fit) effect_from_sim_study(fit))
+summaries_mv_probit5 <- brmsfits5 |>
+    map(\(fit) effect_from_sim_study(fit))
+summaries_mv_probit6 <- brmsfits6 |>
+    map(\(fit) effect_from_sim_study(fit))
+
+all_effects_mv_probit1 <- purrr::map_df(1:length(eff_probs),
+                                       ~ data.frame(id = .x, type = "mv_probit",
+                                                    eff_prob = 0.5, estimate = summaries_mv_probit1[[.x]],
+                                                    maxdelay = "18", lab = "p"))
+all_effects_mv_probit2 <- purrr::map_df(1:length(eff_probs),
+                                        ~ data.frame(id = .x, type = "mv_probit",
+                                                     eff_prob = 0.5, estimate = summaries_mv_probit2[[.x]],
+                                                     maxdelay = "24", lab = "p"))
+all_effects_mv_probit3 <- purrr::map_df(1:length(eff_probs),
+                                        ~ data.frame(id = .x, type = "mv_probit",
+                                                     eff_prob = 0.5, estimate = summaries_mv_probit3[[.x]],
+                                                     maxdelay = "30", lab = "p"))
+all_effects_mv_probit4 <- purrr::map_df(1:length(eff_probs),
+                                        ~ data.frame(id = .x, type = "mv_probit",
+                                                     eff_prob = 0.5, estimate = summaries_mv_probit4[[.x]],
+                                                     maxdelay = "18", lab = "no p"))
+all_effects_mv_probit5 <- purrr::map_df(1:length(eff_probs),
+                                        ~ data.frame(id = .x, type = "mv_probit",
+                                                     eff_prob = 0.5, estimate = summaries_mv_probit5[[.x]],
+                                                     maxdelay = "24", lab = "no p"))
+all_effects_mv_probit6 <- purrr::map_df(1:length(eff_probs),
+                                        ~ data.frame(id = .x, type = "mv_probit",
+                                                     eff_prob = 0.5, estimate = summaries_mv_probit6[[.x]],
+                                                     maxdelay = "30", lab = "no p"))
+
+all_effects_mv_probit <- bind_rows(all_effects_mv_probit1,
+                                   all_effects_mv_probit2,
+                                   all_effects_mv_probit3,
+                                   all_effects_mv_probit4,
+                                   all_effects_mv_probit5,
+                                   all_effects_mv_probit6)
+
+
+summs <- all_effects_mv_probit |>
+    group_by(id, maxdelay, lab) |>
+    summarise(#eff_prob = unique(eff_prob),
+              q_025 = quantile(estimate, 0.025),
+              mean = mean(estimate),
+              q_975 = quantile(estimate, 0.975),
+              flag1 = ifelse(q_025 < 0 & q_975 >0, 1, 0)) |>
+    group_by(maxdelay, lab) |>
+    summarise(coverage = mean(flag1, na.rm = TRUE)) |>
+    arrange(lab, maxdelay)
+summs
+
+
+plot_mv_probit <- all_effects_mv_probit %>%
+    mutate(eff_prob = factor(eff_prob),
+           maxdelay = factor(maxdelay)) %>%
+    ggplot(aes(x =  id, y = estimate)) +
+    geom_hline(yintercept = 0) +
+    stat_pointinterval(position = position_dodge(width = 0.3)) +
+    #    expand_limits(y = shared_range) +
+    ggtitle("MV probit") + facet_wrap(~ maxdelay + lab, ncol = 2)
+
+plot_mv_probit
+
